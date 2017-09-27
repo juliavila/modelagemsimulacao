862f4b07d9795cafa720c00d30bd7e1f55495d2b
Júlia Vila <juliafvila@gmail.com>
2017-09-27 10:46:10 -0300

    fix(simulador): simpleficando a classe lugar.

--- SimuladorRedesDePetri/SimuladorRedesDePetri/Lugar.cpp
+++ SimuladorRedesDePetri/SimuladorRedesDePetri/Lugar.cpp
@@ -17,19 +17,9 @@ int Lugar::getMarcas()
 	return marcas;
 }
 
-list<arco*>* Lugar::getArcos()
-{
-	return &arcos;
-}
-
 void Lugar::setMarca(int m)
 {
 	marcas = m;
-}
-
-void Lugar::setArcos(list<arco*> a)
-{
-	arcos = a;
 }
 
 void Lugar::addMarcas(int m)
@@ -37,17 +27,7 @@ void Lugar::addMarcas(int m)
 	marcas += m;
 }
 
-void Lugar::addArco(arco *a)
-{
-	arcos.push_back(a);
-}
-
 void Lugar::removeMarcas(int m)
 {
 	marcas -= m;
 }
-
-//void Lugar::removeArco(arco a)
-//{
-//	arcos.remove(arcos[ap]);
-//}
--- SimuladorRedesDePetri/SimuladorRedesDePetri/Lugar.h
+++ SimuladorRedesDePetri/SimuladorRedesDePetri/Lugar.h
@@ -2,25 +2,18 @@
 #include <list>
 using namespace std;
 
-class arco;
-
 class Lugar
 {
 private:
 	int marcas;
-	list<arco*> arcos;
 public:
 	Lugar();
 	~Lugar();
 
 	int getMarcas();
-	list<arco*>* getArcos();
 
 	void setMarca(int);
-	void setArcos(list<arco*>);
 
 	void addMarcas(int);
-	void addArco(arco*);
 	void removeMarcas(int);
-	/*void removeArco(int);*/
 };
